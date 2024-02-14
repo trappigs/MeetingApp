4aded16e721fd9c2bf54974cd6431a8272c99e98
Muhammed Hatay <46846228+trappigs@users.noreply.github.com>
2024-02-14 08:42:59 +0300

    MeetingApp/Model Binding/34

--- MeetingApp/Controllers/MeetingController.cs
+++ MeetingApp/Controllers/MeetingController.cs
@@ -4,14 +4,30 @@ namespace MeetingApp.Controllers
 {
     public class MeetingController : Controller
     {
+
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
         public IActionResult Apply()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Apply(string Name, string Phone, string Email, bool WillAttend)
+        {
+            System.Diagnostics.Debug.WriteLine(Name+"ahahahasad");
+            System.Diagnostics.Debug.WriteLine(Phone);
+
+            return View();
+        }
+
+        [HttpGet]
         public IActionResult List()
         {
             return View();
